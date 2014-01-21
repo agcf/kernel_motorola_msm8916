cdb89b679bd80e42d7c4b7d0c1f4d1db6934ec3d
Oleg Nesterov <oleg@redhat.com>
2014-01-21 15:50:00 -0800

    oom_kill: has_intersects_mems_allowed() needs rcu_read_lock()
    commit ad96244179fbd55b40c00f10f399bc04739b8e1f upstream.
    
    At least out_of_memory() calls has_intersects_mems_allowed() without
    even rcu_read_lock(), this is obviously buggy.
    
    Add the necessary rcu_read_lock().  This means that we can not simply
    return from the loop, we need "bool ret" and "break".
    
    While at it, swap the names of task_struct's (the argument and the
    local).  This cleans up the code a little bit and avoids the unnecessary
    initialization.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Sergey Dyasly <dserrg@gmail.com>
    Tested-by: Sergey Dyasly <dserrg@gmail.com>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mandeep Singh Baines <msb@chromium.org>
    Cc: "Ma, Xindong" <xindong.ma@intel.com>
    Reviewed-by: Michal Hocko <mhocko@suse.cz>
    Cc: "Tu, Xiaobing" <xiaobing.tu@intel.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Li Zefan <lizefan@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -63,6 +63,7 @@ static bool has_intersects_mems_allowed(struct task_struct *start,
 	struct task_struct *tsk;
 	bool ret = false;
 
+	rcu_read_lock();
 	for_each_thread(start, tsk) {
 		if (mask) {
 			/*
@@ -79,7 +80,10 @@ static bool has_intersects_mems_allowed(struct task_struct *start,
 			 */
 			ret = cpuset_mems_allowed_intersects(current, tsk);
 		}
+		if (ret)
+			break;
 	}
+	rcu_read_unlock();
 
 	return ret;
 }
